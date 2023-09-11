cdac8c3b200926ba645bd5d1e2870e6046baa78d
aliazeemabbas <aliazeem480@gmail.com>
2023-09-11 21:48:52 +0500

    error handling

--- controllers/main.js
+++ controllers/main.js
@@ -16,22 +16,13 @@ const login = async (req, res) => {
 
 const dashboard = async (req, res) => {
 
-    const authHeader = req.headers.authorization;
-    if(!authHeader || !authHeader.startsWith('Bearer')){
-        throw new CustomAPIError('No token', 401)
-    }
-
-    const token = authHeader.split(' ')[1]
+    
     // console.log(token)
 
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
         const luckyNumber = Math.floor(Math.random()*100)
-        res.status(200).json({msg: `Hello ${decoded.username}`, secret:`Here is your authorized data, 
+
+        res.status(200).json({msg: `Hello ${req.user.username}`, secret:`Here is your authorized data, 
             your lucky number is ${luckyNumber}`})
-    } catch (error) {
-        throw new CustomAPIError('Not authorized', 401)
-    }
     
 }
 
