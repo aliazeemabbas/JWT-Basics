330a82e677cf19e338cbb5dcdc1176b9867a21df
aliazeemabbas <aliazeem480@gmail.com>
2023-09-11 22:04:44 +0500

    added http error codes

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,11 @@
 const jwt = require('jsonwebtoken')
-const CustomAPIError = require("../errors/custom-error");
+const {UnauthenticatedError} = require('../errors');
 
 const authenticationMiddleware = async (req, res, next) => {
    
     const authHeader = req.headers.authorization;
     if(!authHeader || !authHeader.startsWith('Bearer')){
-        throw new CustomAPIError('No token', 401)
+        throw new UnauthenticatedError('No token')
     }
 
     const token = authHeader.split(' ')[1]
@@ -16,7 +16,7 @@ const authenticationMiddleware = async (req, res, next) => {
         req.user = {id, username}
         next()
     } catch (error) {
-        throw new CustomAPIError('Not authorized', 401)
+        throw new UnauthenticatedError('Not authorized')
     }
 
     next()
